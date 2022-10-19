d96242748d029f76e71d64525882e56e6ba39f5a
forestheims <forestheims@gmail.com>
2022-10-18 19:03:00 -0700

    inputs now controlled for write blog form.

--- src/components/Blog/Write/Write.jsx
+++ src/components/Blog/Write/Write.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
+import useForm from '../../../hooks/useForm';
 import styles from './Write.css';
 
 export default function Write() {
+  const { formState, formError, handleFormChange, setFormError } = useForm({
+    title: '',
+    content: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('formState :>> ', formState);
+  };
+
   return (
-    <form className={styles.Write}>
+    <form className={styles.Write} onSubmit={(e) => handleSubmit(e)}>
       <label htmlFor="title">
         Title
-        <input type="text" />
+        <input
+          type="text"
+          name="title"
+          value={formState.title}
+          onChange={(e) => handleFormChange(e)}
+        />
       </label>
       <label htmlFor="content">
-        <input type="textarea" />
+        <textarea
+          name="content"
+          value={formState.content}
+          onChange={(e) => handleFormChange(e)}
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
