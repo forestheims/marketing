adb9f4f3f418435cfd06f5fc48f22860cb3a435a
forestheims <forestheims@gmail.com>
2022-10-20 19:59:51 -0700

    add framer-motion and animate divs on Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,41 @@
 import styles from './Home.css';
+import { useRef } from 'react';
+import { motion } from 'framer-motion';
+import { useFollowPointer } from './useFollowPointer.js';
 
 export default function Home() {
+  const ref = useRef(null);
+  const { x, y } = useFollowPointer(ref);
+  const variants = {
+    entryPage: (i) => ({
+      x: x + Math.cos(1),
+      y: y + Math.sin(1),
+      transition: {
+        type: 'spring',
+        damping: i + 1,
+        stiffness: i + 1,
+        restDelta: 0.01 * i,
+      },
+    }),
+  };
+
+  let anArray = new Array(33).fill('');
+
   return (
-    <main className={styles.Home}>
-      <p>Hello World</p>
-    </main>
+    <div className={styles.Home}>
+      {anArray.map((value, i) => (
+        <motion.div
+          key={i}
+          className={styles.thirtyThree}
+          custom={i}
+          animate="entryPage"
+          variants={variants}
+          value={value}
+          ref={ref}
+          whileHover={{ color: 'rgb(45, 120, 98)' }}
+          whileTap={{ opacity: 0 }}
+        ></motion.div>
+      ))}
+    </div>
   );
 }
