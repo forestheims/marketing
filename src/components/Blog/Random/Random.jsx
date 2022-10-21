cff9f9f4ca19904fb345e6e64733e1dc9468968f
forestheims <forestheims@gmail.com>
2022-10-20 20:08:04 -0700

    temporary faux content in Blog detail view for css

--- src/components/Blog/Random/Random.jsx
+++ src/components/Blog/Random/Random.jsx
@@ -6,10 +6,11 @@ export default function Random() {
   const { blogId } = useParams();
   const [blog, setBlog] = useState({
     id: '',
-    title: '',
+    title: 'wrods',
     createdAt: '',
-    tags: [],
-    body: [],
+    tags: ['this', 'that', 'the'],
+    content:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum \n\n "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
   });
 
   useEffect(() => {
@@ -18,7 +19,7 @@ export default function Random() {
         .then((response) => response.json())
         .then((data) => setBlog(data));
     };
-    // expect data to be in shape: {id, title, createdAt, tags, body}
+    // expect data to be in shape: {id, title, createdAt, tags, content}
     // fetchData();
   }, []);
 
@@ -27,10 +28,13 @@ export default function Random() {
       <h1>{blog.title}</h1>
       <ul>
         {blog.tags.map((tag) => (
-          <li key={Math.random(100)}></li>
+          <li key={Math.random(100)}>{tag}</li>
         ))}
       </ul>
-      <p>BlogId = {blogId}</p>
+      <p>{blog.content}</p>
+      {/* {content.split('\n\n').map((paragraph) => {
+        return <p>{paragraph}</p>
+      })} */}
     </div>
   );
 }
