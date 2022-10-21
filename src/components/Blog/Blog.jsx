a08f69e4245219f3fb0b6c55933bfebb9a9c65ae
forestheims <forestheims@gmail.com>
2022-10-20 20:03:41 -0700

    import css files for more explicit css, update css

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,7 +4,7 @@ import styles from './Blog.css';
 export default function Blog() {
   return (
     <div className={styles.Blog}>
-      <div>
+      <div className={styles.BlogDiv}>
         <NavLink to="">Browse</NavLink>
         <NavLink to="write">Write</NavLink>
         <NavLink to="f43gw4">Random</NavLink>
