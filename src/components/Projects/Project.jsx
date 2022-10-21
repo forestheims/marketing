1eceae6286974920a10488e57ffd3abe39863f88
forestheims <forestheims@gmail.com>
2022-10-20 20:06:00 -0700

    remove console log and update Projects view css

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,7 +1,6 @@
 import styles from './Projects.css';
 
 export default function Project({ project }) {
-  console.log('project', project);
   return (
     <div className={styles.Project}>
       <p>{project.anime_name}</p>
