66b986d5b25fb98d53856e937623d5c07333e9ab
forestheims <forestheims@gmail.com>
2022-10-23 12:32:37 -0700

    relocated DivDots entry point to NavBar component

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,23 +1,32 @@
 import { NavLink } from 'react-router-dom';
 import { useTheme } from '../../context/themeProvider';
+import DivDots from '../DivDots/DivDots';
 import styles from './Layout.css';
 
 export default function NavBar() {
   const { theme, setTheme } = useTheme();
 
+  const handleTheme = (e) => {
+    if (theme.darkMode) setTheme({ ...theme, darkMode: false });
+    else setTheme({ ...theme, darkMode: true });
+  };
+
   const handleClick = (e) => {
-    if (theme === 'light') setTheme('dark');
-    else setTheme('light');
+    if (theme.divDots) setTheme({ ...theme, divDots: false });
+    else setTheme({ ...theme, divDots: true });
   };
 
   return (
     <header>
+      <div className={styles.DivDotsCircle} onClick={(e) => handleClick(e)}>
+        {theme.divDots && <DivDots />}
+      </div>
       <NavLink to="">Home</NavLink>
       <NavLink to="blog">Blog</NavLink>
       <NavLink to="projects">Projects</NavLink>
       <NavLink to="contact">Contact</NavLink>
-      <div className={styles.circle}>
-        <img onClick={(e) => handleClick(e)} src="/images/5558220.png"></img>
+      <div className={styles.Circle}>
+        <img onClick={(e) => handleTheme(e)} src="/images/5558220.png"></img>
       </div>
     </header>
   );
