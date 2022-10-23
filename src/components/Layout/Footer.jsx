b64094df6f777e04534ff76b66c7964149975e3c
forestheims <forestheims@gmail.com>
2022-10-23 12:28:15 -0700

    update the theme provider to provide more state

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
   return (
     <footer>
-      <p>{theme}</p>
+      <p>{theme.darkMode && 'dark mode on'}</p>
     </footer>
   );
 }
--- src/context/themeProvider.jsx
+++ src/context/themeProvider.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState({ darkMode: true, divDots: true });
 
   const contextValue = { theme, setTheme };
 
