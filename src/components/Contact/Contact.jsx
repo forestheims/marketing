906dadc272e95d6a4e0ce10c53153fb419d4f9c4
forestheims <forestheims@gmail.com>
2022-10-22 17:47:01 -0700

    add content and styling to Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,51 @@
 import styles from './Contact.css';
+import linkedInLogo from '../../../public/images/LI-In-Bug.png';
+import gitHubLogo from '../../../public/images/GitHub-Mark-Light-64px.png';
+import pdfPng from '../../../public/images/pdf.png';
+import resumeDisco from '../../../public/resume/resume-disco.mp4';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function Contact() {
+  const [animating, setAnimating] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => void setAnimating(false), 5500);
+  }, []);
+
   return (
-    <main className={styles.Contact}>
-      <p>Contact</p>
-    </main>
+    <div className={styles.Contact}>
+      {animating && (
+        <motion.iframe
+          className={styles.changingPdf}
+          src={resumeDisco}
+          animate={{
+            scale: [0.1, 1, 1, 1, 1, 0.75, 0.1],
+            x: [0, 0, 0, 0, 0, 200],
+            y: [-20, -20, -20, -20, -20, -320],
+            borderRadius: ['0%', '0%', '0%', '0%', '20%', '20%'],
+          }}
+          transition={{
+            duration: 4.5,
+          }}
+        ></motion.iframe>
+      )}
+      <a href="/resume/dark-mode-resume.pdf" target="_blank">
+        <img src={pdfPng} alt="pdf symbol" />
+        View / Download my Resume
+      </a>
+      <a href="mailto:forestheims@gmail.com" target="_blank">
+        <h2>📧</h2>
+        forestheims@gmail.com
+      </a>
+      <a href="https://github.com/forestheims" target="_blank">
+        <img src={gitHubLogo} alt="github logo" />
+        github.com/forestheims
+      </a>
+      <a href="https://www.linkedin.com/in/forestheims/" target="_blank">
+        <img src={linkedInLogo} alt="linkedin logo" />
+        linkedin.com/in/forestheims
+      </a>
+    </div>
   );
 }
