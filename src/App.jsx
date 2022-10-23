0ee465f16a9edd36d3045c831936b1f29b0c9664
forestheims <forestheims@gmail.com>
2022-10-23 12:25:08 -0700

    remove Home Component and add About component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import './global.css';
 import './reset.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Contact from './components/Contact/Contact';
-import Home from './components/Home/Home';
 import Layout from './components/Layout/Layout';
 import Random from './components/Blog/Random/Random';
 import Write from './components/Blog/Write/Write';
 import Browse from './components/Blog/Browse/Browse';
 import Blog from './components/Blog/Blog';
 import Projects from './components/Projects/Projects';
+import About from './components/About/About';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<About />} />
           <Route path="blog" element={<Blog />}>
             <Route index element={<Browse />} />
             <Route path="write" element={<Write />} />
---
+++ src/components/About/About.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './About.css';
+
+export default function About() {
+  return <div className={styles.About}></div>;
+}
