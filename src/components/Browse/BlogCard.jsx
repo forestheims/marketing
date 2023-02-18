30515be8fedb3a5bcfa0045aa83388526cd44860
forestheims <forestheims@gmail.com>
2023-02-18 12:23:29 -0800

    add another placeholder blog topic to make use of the scroll feature that was there.

--- src/components/Browse/BlogCard.jsx
+++ src/components/Browse/BlogCard.jsx
@@ -6,6 +6,23 @@ export default function BlogCard({ blog }) {
   const navigate = useNavigate();
   const splitContent = content.split('<br />');
 
+  const scanForElements = (stringP) => {
+    let splitP = stringP.split('<a>');
+    if (splitP.length > 1) {
+      let urlP = splitP[1].split('</a>')[0];
+      return (
+        <a key={urlP} className={styles.BlogCardLink} href={urlP}>
+          {urlP}
+        </a>
+      );
+    } else
+      return (
+        <p key={stringP} className={styles.BlogCardContent}>
+          {stringP}
+        </p>
+      );
+  };
+
   return (
     <div className={styles.BlogCard} onClick={() => navigate(`/blog/${id}`)}>
       <img className={styles.BlogCardImg} src={imageURL} alt={title} />
@@ -15,9 +32,7 @@ export default function BlogCard({ blog }) {
         <span className={styles.BlogCardDate}>{createdAt}</span>
         <span className={styles.BlogCardTag}>{tags[0]}</span>
       </div>
-      {splitContent.map((p) => (
-        <p className={styles.BlogCardContent}>{p}</p>
-      ))}
+      {splitContent.map((p) => scanForElements(p))}
       {/* </div> */}
     </div>
   );
--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -5,6 +5,16 @@ import styles from './Browse.css';
 export default function () {
   const [blogs, setBlogs] = useState([
     {
+      id: '8',
+      title: 'The Library of Babel',
+      createdAt: '2/18/2023',
+      tags: ['philosophy of language', 'this one is for you John Powell'],
+      imageURL:
+        'https://images.squarespace-cdn.com/content/6292e4c9c6759c50bca53dbd/1657981182457-V38MIWM1KC6YC1L2V1S2/20220716+Borges+Library+blog.jpg?format=1500w&content-type=image%2Fjpeg',
+      content:
+        '<a>https://maskofreason.files.wordpress.com/2011/02/the-library-of-babel-by-jorge-luis-borges.pdf</a>',
+    },
+    {
       id: '1',
       title: 'Tech Talk',
       createdAt: '2/17/2023',
